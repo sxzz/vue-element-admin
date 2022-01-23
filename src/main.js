5a8f930abb3c525a4d501cc647d01b06779fa2bf
Kevin <sxzz@sxzz.moe>
2022-01-24 04:15:55 +0800

    createApp

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 
 import Cookies from 'js-cookie'
 
 import 'normalize.css/normalize.css' // a modern alternative to CSS resets
 
-import Element from 'element-plus'
+import ElementPlus from 'element-plus'
 import './styles/element-variables.scss'
 import enLang from 'element-plus/es/locale/lang/en'// 如果使用中文语言包请默认支持，无需额外引入，请删除该依赖
 
@@ -17,8 +17,6 @@ import router from './router'
 import './icons' // icon
 import './permission' // permission control
 import './utils/error-log' // error log
-
-import * as filters from './filters' // global filters
 
 /**
  * If you don't want to use mock-server
@@ -33,21 +31,7 @@ if (process.env.NODE_ENV === 'production') {
   mockXHR()
 }
 
-Vue.use(Element, {
-  size: Cookies.get('size') || 'medium', // set element-ui default size
+createApp(App).use(store).use(router).use(ElementPlus, {
+  size: Cookies.get('size') || 'default',
   locale: enLang // 如果使用中文，无需设置，请删除
-})
-
-// register global utility filters
-Object.keys(filters).forEach(key => {
-  Vue.filter(key, filters[key])
-})
-
-Vue.config.productionTip = false
-
-new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App)
-})
+}).mount('#app')
